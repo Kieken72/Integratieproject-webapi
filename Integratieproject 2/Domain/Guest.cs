06c665b4a8ad58a33edd6738b993b90f91cfa05a
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-02-04 18:19:21 +0100

    Domain

---
+++ Integratieproject 2/Domain/Guest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leisurebooker.Business.Domain
+{
+    public class Guest : IntEntity
+    {
+        //Klant gegevens apart van login? (Makkelijker opslaanbaar?)
+    }
+}
