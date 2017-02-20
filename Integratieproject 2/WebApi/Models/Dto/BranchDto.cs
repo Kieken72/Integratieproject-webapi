98466afb0177a6bd8b2545b99c54a6ce92fe58b3
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-02-20 11:47:30 +0100

    UPDATE DTO

--- Integratieproject 2/WebApi/Models/Dto/BranchDto.cs
+++ Integratieproject 2/WebApi/Models/Dto/BranchDto.cs
@@ -15,6 +15,7 @@ namespace WebApi.Models.Dto
         public CityDto City { get; set; }
 
         public string Picture { get; set; }
+        public string Description { get; set; }
 
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
