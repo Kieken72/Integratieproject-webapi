c3db78acbe31cf453c6732a7d97d4c2738307a6a
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-03-02 10:17:14 +0100

    Tests

---
+++ Integratieproject 2/DataAccess.Tests/EF/Repositories/MessagesRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Leisurebooker.DataAccess.Tests.EF.Repositories
+{
+    [TestFixture]
+    public class MessagesRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
---
+++ Integratieproject 2/DataAccess.Tests/EF/Repositories/ReviewRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Leisurebooker.Business.Domain;
+using Leisurebooker.DataAccess.Tests.Fakes;
+using NUnit.Framework;
+
+namespace Leisurebooker.DataAccess.Tests.EF.Repositories
+{
+    [TestFixture]
+    public class ReviewRepositoryTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+    }
+}
