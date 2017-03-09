8fb0a9a05b9b13b1ee28878f7346e5e3e2130812
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-03-09 15:30:25 +0100

    FullAcc

--- Integratieproject 2/WebApi/Models/Dto/AccountDto.cs
+++ Integratieproject 2/WebApi/Models/Dto/AccountDto.cs
@@ -11,6 +11,14 @@ namespace WebApi.Models.Dto
         public string Lastname { get; set; }
     }
 
+    public class FullAccountDto
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
     //public class AccountDto
     //{
     //    public int Id { get; set; }
