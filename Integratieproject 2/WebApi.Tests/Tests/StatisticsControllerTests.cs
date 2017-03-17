c7de9f1ad8f0286253c6f832e731c133987e5c19
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-03-17 10:22:07 +0100

    Statistics..

---
+++ Integratieproject 2/WebApi.Tests/Tests/StatisticsControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApi.Tests.Tests
+{
+    [TestFixture]
+    public class StatisticsControllerTests
+    {
+        private StatisticsControllerTests _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+         
+            _controller = new StatisticsControllerTests();   
+        }
+    }
+}
---
+++ Integratieproject 2/WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Leisurebooker.Business;
+using Leisurebooker.Business.Domain;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/branches")]
+    [Authorize(Roles = "Manager")]
+    public class StatisticsController : ApiController
+    {
+        private readonly IService<Branch> _branchService;
+        private readonly IService<Reservation> _reservationService;
+        private readonly IService<Review> _reviewService;
+
+        public StatisticsController(IService<Branch> service, IService<Reservation> reservationService, IService<Review> reviewService )
+        {
+            _branchService = service;
+            _reservationService = reservationService;
+            _reviewService = reviewService;
+        }
+
+        [Route("reviews/{id}")]
+        public IHttpActionResult ReviewStatistics(int id)
+        {
+            var entity = this._reviewService.Get(e => e.BranchId == id, collections: true);
+
+            if (entity == null) return Ok();
+            var amount = entity.Count();
+            var positive = entity.Count(e => e.Result);
+            var negative = amount - positive;
+
+            return Ok(new { amount, positive, negative });
+        }
+
+        [Route("weekday/{id}")]
+        public IHttpActionResult WeekdayStatistics(int id)
+        {
+            var entity = this._reservationService.Get(e => e.BranchId == id, collections: true);
+
+            if (entity == null) return Ok();
+
+            //For each weekday..
+            //
+            return Ok();
+        }
+    }
+}
